4a7025bfc2d223c72aa8c2e3f2182a17e3928588
daigo-007 <kairibeetle007@gmail.com>
2019-11-19 12:31:16 +0900

    added to-do

---
+++ to-do/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import './index.css';
+import Todo from './todo';
+import * as serviceWorker from './serviceWorker';
+ReactDOM.render(<Todo />, document.getElementById('root'));
+serviceWorker.unregister();
